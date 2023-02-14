ffbb6b335fda2e623388bc12e86df696129378ce
Hogoww <pierre_misse25@msn.com>
2023-02-14 07:21:40 +0100

    [cleanup] git remove some files, update gitignore

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "memorySpace.h"
-
-int main(){
-  uint64_t b = 640;
-  MemorySpace* a = new MemorySpace(b);
-  std::cout<< a->getStartAddress() << std::endl;
-  return 1;
-}
