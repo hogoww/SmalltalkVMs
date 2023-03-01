5daec85088e5baf46726acc6de37b4e6f92b58f6
Hogoww <pierre_misse25@msn.com>
2023-03-01 04:35:38 +0100

    [improvement] basic GC test, bit of cleanup

---
+++ tests/testGarbageCollection/testGarbageCollectionDoesNotReclaimSlotOfRoot.cpp
@@ -0,0 +1,24 @@
+#include "memorySpace.hpp"
+#include "cTestCase.h"
+#include <vector>
+
+#ifndef WORD_TYPE
+#define WORD_TYPE uint64_t
+#endif
+
+int main(){
+  MemorySpace<WORD_TYPE> ms(640);
+  OopBuilder<WORD_TYPE>* oopBuilder = ms.getOopBuilder();
+  std::vector<WORD_TYPE*> roots(2);
+  oopBuilder -> setNumberOfSlots(1);
+  roots.push_back(oopBuilder -> build());
+  
+  oopBuilder -> reset();
+  ms.firstOop().slotAtPut(1, oopBuilder -> build());
+  
+  ms.getGarbageCollector() -> collectFromRoots(roots);
+  
+  cAssert(__LINE__, not ms.firstOop().nextOop().isFreeOop());
+  testPassed();
+}
+
---
+++ tests/testGarbageCollection/testGarbageCollectionReclaimsAllObjectsWithoutRoots.cpp
@@ -0,0 +1,19 @@
+#include "memorySpace.hpp"
+#include "cTestCase.h"
+#include <vector>
+
+#ifndef WORD_TYPE
+#define WORD_TYPE uint64_t
+#endif
+
+int main(){
+  MemorySpace<WORD_TYPE> ms(640);
+  OopBuilder<WORD_TYPE>* oopBuilder = ms.getOopBuilder();
+  std::vector<WORD_TYPE*> roots(0);
+  oopBuilder -> build();
+  
+  ms.getGarbageCollector() -> collectFromRoots(roots);
+  
+  cAssert(__LINE__, ms.firstOop().isFreeOop());
+  testPassed();
+}
