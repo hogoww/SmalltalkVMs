773b5a9fb5e079ef94e603a6d8bd24f7af899495
Hogoww <pierre_misse25@msn.com>
2023-05-04 17:08:34 +0200

    [fix] Initialize of vector for a test

--- tests/testGarbageCollection/testMarkSlotOfRoot.cpp
+++ tests/testGarbageCollection/testMarkSlotOfRoot.cpp
@@ -9,7 +9,7 @@
 int main(){
   MemorySpace<WORD_TYPE> ms(640);
   OopBuilder<WORD_TYPE>* oopBuilder = ms.getOopBuilder();
-  std::vector<WORD_TYPE*> roots;
+  std::vector<WORD_TYPE*> roots(0);
   oopBuilder -> setNumberOfSlots(1);
   roots.push_back(oopBuilder -> build());
   
